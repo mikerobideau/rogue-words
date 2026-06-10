e739cf25a4cbb77fe06087ab3a59d2251631f433
Mike Robideau <kingrobideau@gmail.com>
2026-06-10 17:03:56 -0400

    fix bug related to toast timeout before queue free

--- components/score_toast/score_toast.gd
+++ components/score_toast/score_toast.gd
@@ -8,8 +8,10 @@ class_name ScoreToast
 
 func _ready() -> void:
 	label.text = text
-	position = Vector2(get_viewport_rect().size.x / 2, 50)
+	position = Vector2(get_viewport_rect().size.x / 2, 100)
 	label.size = label.get_minimum_size()
 	background.size = label.size + Vector2(20, 0)
 	background.position = -background.size / 2
 	label.position = -label.size / 2
+	await get_tree().create_timer(0.5).timeout
+	queue_free()
