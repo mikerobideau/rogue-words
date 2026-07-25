6e2cfa80e2ff74c5046653adf405c799b082c457
Mike Robideau <kingrobideau@gmail.com>
2026-07-25 19:43:49 -0400

    slide in side panel

--- components/score/score_panel.gd
+++ components/score/score_panel.gd
@@ -11,15 +11,8 @@ var x_out: int
 var slide_distance: int
 
 func _ready():
-	print_debug('score panel ready')
+	modulate.a = 0.0
 	pivot_offset = size / 2
-	_init_pos()
-	
-func _init_pos():
-	slide_distance = size.x
-	x_in = 0
-	x_out = x_in - slide_distance
-	position.x = x_out
 
 var score := 0:
 	set(v):	
@@ -42,9 +35,14 @@ var turns_remaining: int:
 func target_met():
 	return score >= target_score
 	
-func slide_in():
-	await get_tree().create_timer(0.5).timeout
+func slide_in(delay := 0.5):
+	await get_tree().process_frame
+	x_in = position.x
+	slide_distance = size.x + 50
+	x_out = x_in - slide_distance
 	position.x = x_out
+	modulate.a = 1.0
+	await get_tree().create_timer(delay).timeout
 	var tween = create_tween()
 	tween.set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
 	tween.tween_property(self, 'position:x', x_in, 0.5)
